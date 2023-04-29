29898cc9edbbf6dd366d56d9d94af17436130b41
Sahil <sahilgirdhar410@gmail.com>
2023-04-29 03:08:55 -0400

    added settings configuration to control diff qa/prod/local env

--- manage.py
+++ manage.py
@@ -6,7 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BookGpt.settings')
+    env = os.environ.get('DJANGO_ENV', 'local')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', f'BookGpt.settings.{env}')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
